c628e6daec504ec6aa48e89b6d0213becf386810
galadriel91 <g_ala@naver.com>
2023-08-14 18:06:26 +0900

    <Update: Define Content Area>

--- 01.World/js/script.js
+++ 01.World/js/script.js
@@ -4,4 +4,21 @@ jQuery(document).ready(function(){
     }).mouseout(function(){
         $(this).find('.submenu').stop().slideUp(500);
     })
+
+    $('.imgSlide a:gt(0)').hide();
+    setInterval(()=>{
+        $('.imgSlide a:first-child')
+        .fadeOut(1000)
+        .next('a')
+        .fadeIn(1000)
+        .end()
+        .appendTo('.imgSlide')
+    }, 3000)
+
+    $('.notice li:first').click(function(){
+        $('#modal').addClass('active')
+    })
+    $('.btn').click(function(){
+        $('#modal').removeClass('active')
+    })
 })
